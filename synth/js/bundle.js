b625720e91a6e534a89b2b9b66ad1478d5783a8c
Luis Crespo <lcrespom@gmail.com>
2015-12-26 09:18:39 +0100

    Added presets

--- synth/js/bundle.js
+++ synth/js/bundle.js
@@ -58,6 +58,9 @@
 	function setupKeyboard() {
 	    var kb = new keyboard_1.Keyboard();
 	    kb.noteOn = function (midi, ratio) {
+	        if (document.activeElement.nodeName == 'INPUT' &&
+	            document.activeElement.getAttribute('type') != 'range')
+	            return;
 	        synthUI.synth.noteOn(midi, 1, ratio);
 	    };
 	    kb.noteOff = function (midi) {
@@ -541,6 +544,8 @@
 /* 4 */
 /***/ function(module, exports) {
 
+	var SHIFT_KEY = 16;
+	var CAPS_LOCK = 20;
 	/**
 	 * A generic directed graph editor.
 	 */
@@ -619,8 +624,7 @@
 	                y: node.y,
 	                name: node.name,
 	                inputs: nodeInputs,
-	                classes: node.element.attr('class')
-	                    .split(' ').filter(function (v) { return v.substr(0, 3) != 'ui-'; }).join(' ')
+	                classes: this.getAppClasses(node)
 	            });
 	            jsonNodeData.push(this.handler.data2json(node));
 	        }
@@ -676,6 +680,19 @@
 	                return node;
 	        }
 	        return null;
+	    };
+	    Graph.prototype.getAppClasses = function (n) {
+	        var classes = n.element[0].className.split(/\s+/);
+	        var result = [];
+	        for (var _i = 0; _i < classes.length; _i++) {
+	            var cname = classes[_i];
+	            if (cname == 'selected')
+	                continue;
+	            if (cname.substr(0, 3) == 'ui-')
+	                continue;
+	            result.push(cname);
+	        }
+	        return result.join(' ');
 	    };
 	    return Graph;
 	})();
@@ -771,7 +788,9 @@
 	        var srcn;
 	        var connecting = false;
 	        $('body').keydown(function (evt) {
-	            if (evt.keyCode != 16 || connecting)
+	            if (evt.keyCode == CAPS_LOCK)
+	                return _this.setGrid([20, 20]);
+	            if (evt.keyCode != SHIFT_KEY || connecting)
 	                return;
 	            srcn = _this.getNodeFromDOM(_this.getElementUnderMouse());
 	            if (!srcn)
@@ -784,7 +803,9 @@
 	            _this.registerRubberBanding(srcn);
 	        })
 	            .keyup(function (evt) {
-	            if (evt.keyCode != 16)
+	            if (evt.keyCode == CAPS_LOCK)
+	                return _this.setGrid(null);
+	            if (evt.keyCode != SHIFT_KEY)
 	                return;
 	            connecting = false;
 	            _this.deregisterRubberBanding();
@@ -794,6 +815,9 @@
 	            _this.connectOrDisconnect(srcn, dstn);
 	            _this.graph.draw();
 	        });
+	    };
+	    GraphInteraction.prototype.setGrid = function (grid) {
+	        $(this.graph.nodeCanvas).find('.node').draggable("option", "grid", grid);
 	    };
 	    GraphInteraction.prototype.connectOrDisconnect = function (srcn, dstn) {
 	        if (this.graph.disconnect(srcn, dstn))
